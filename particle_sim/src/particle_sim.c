e4eb80930160be3951f43b983d1f803b50f6ee72
amritpal-sidhu <amritpalsidhu.p@gmail.com>
2022-04-21 01:09:34 -0700

    Fix viewport crashing
    Add event polling to window_loop as the glfw tutorial suggested

--- particle_sim/src/particle_sim.c
+++ particle_sim/src/particle_sim.c
@@ -8,17 +8,17 @@
 
 
 void error_callback(int error, const char* description);
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 void window_loop(GLFWwindow* window);
 
 int main(void)
 {
+    glfwSetErrorCallback(error_callback);
     assert(glfwInit());
 
-    glfwSetErrorCallback(error_callback);
-
-    GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Particle Sim", NULL, NULL);
     assert(window);
-
+    glfwSetKeyCallback(window, key_callback);
     glfwMakeContextCurrent(window);
 
     window_loop(window);
@@ -35,9 +35,23 @@ void error_callback(int error, const char* description)
     fprintf(stderr, "Error: %s\n", description);
 }
 
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
 void window_loop(GLFWwindow* window)
 {
     while (!glfwWindowShouldClose(window)) {
         
+        float ratio;
+        int width, height;
+ 
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float) height;
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
 }

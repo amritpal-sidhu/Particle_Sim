cd1e7c7312bc28705df06c00a4a8a20e2f72f017
amritpal-sidhu <amritpalsidhu.p@gmail.com>
2022-04-28 01:34:32 -0700

    Update cmake
    Working on cleaning up cmake files

--- build.sh
+++ build.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-cmake -D CMAKE_TOOLCHAIN_FILE=Third-Party/glfw/CMake/x86_64-w64-mingw32.cmake -S . -B build
+build_type="-D CMAKE_BUILD_TYPE=Debug"
+source_dir="${PWD}"
+build_dir="${PWD}/build"
+cmake_cache_file="${build_dir}/CMakeCache.txt"
+toolchain_file="--toolchain ${PWD}/cmake/x86_64-w64-mingw32.cmake"
+log_level="--log-level=VERBOSE"
+
+
+check_toolchain()
+{
+    cached_toolchain=grep CMAKE_TOOLCHAIN_FILE "$cmake_cache_file" | cut -d "=" -f2
+    return [ "$toolchain_file" = "$cached_toolchain" ]
+}
+
+
+if [[ -f $cmake_cache_file && check_toolchain ]]; then
+    cmake -S $source_dir -B $build_dir $build_type $log_level
+else
+    cmake $toolchain_file -S $source_dir -B $build_dir  $build_type $log_level
+fi
+
 cmake --build build

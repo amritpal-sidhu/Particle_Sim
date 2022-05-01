4e77fea23beba702ef3ee23479decf0777b34c99
amritpal-sidhu <amritpalsidhu.p@gmail.com>
2022-05-01 10:27:21 -0700

    Fix build.sh
    Corrected toolchain_file variable

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ build_type="-D CMAKE_BUILD_TYPE=Debug"
 source_dir="${PWD}"
 build_dir="${PWD}/build"
 cmake_cache_file="${build_dir}/CMakeCache.txt"
-toolchain_file="--toolchain ${PWD}/cmake/x86_64-w64-mingw32.cmake"
+toolchain_file="${PWD}/cmake/x86_64-w64-mingw32.cmake"
 log_level="--log-level=VERBOSE"
 
 
@@ -18,7 +18,7 @@ check_toolchain()
 if [[ -f $cmake_cache_file && check_toolchain ]]; then
     cmake -S $source_dir -B $build_dir $build_type $log_level
 else
-    cmake $toolchain_file -S $source_dir -B $build_dir  $build_type $log_level
+    cmake --toolchain $toolchain_file -S $source_dir -B $build_dir  $build_type $log_level
 fi
 
 cmake --build build

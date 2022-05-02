431ce7816c71ee37d274f320e601d7ee3088cbb9
amritpal-sidhu <amritpalsidhu.p@gmail.com>
2022-05-02 13:59:04 -0700

    Fix function variable assignment to use command substitution

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 build_type="-D CMAKE_BUILD_TYPE=Debug"
 source_dir="${PWD}"
-build_dir="${PWD}/build"
+build_dir="${PWD}/_build"
 cmake_cache_file="${build_dir}/CMakeCache.txt"
 toolchain_file="${PWD}/cmake/x86_64-w64-mingw32.cmake"
 log_level="--log-level=VERBOSE"
@@ -10,7 +10,7 @@ log_level="--log-level=VERBOSE"
 
 check_toolchain()
 {
-    cached_toolchain=grep CMAKE_TOOLCHAIN_FILE "$cmake_cache_file" | cut -d "=" -f2
+    cached_toolchain=$(grep CMAKE_TOOLCHAIN_FILE "$cmake_cache_file" | cut -d "=" -f2)
     return [ "$toolchain_file" = "$cached_toolchain" ]
 }
 
@@ -21,4 +21,4 @@ else
     cmake --toolchain $toolchain_file -S $source_dir -B $build_dir  $build_type $log_level
 fi
 
-cmake --build build
+cmake --build $build_dir

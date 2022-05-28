cb051af22afbf6f977c79b176a60a4eb8c22ff26
asidhu77 <amritpalsidhu.p@gmail.com>
2022-05-28 11:23:04 -0700

    Add help message for build script

--- build.sh
+++ build.sh
@@ -11,15 +11,24 @@ options=("-D CMAKE_BUILD_TYPE=$build_type")
 
 case $1 in
 
-"rm")  rm -rf $build_dir
-;;
-
-"clean")  cmake --build $build_dir --target clean
-;;
-
-*)
+"")
     cmake -G "$generator" -S $source_dir -B $build_dir ${options[*]}
     cmake --build $build_dir
+;;
+
+"-c"|"--clean")  cmake --build $build_dir --target clean
+;;
+
+"-d"|"--delete")  rm -rf $build_dir
+;;
+
+"-h"|"--help"|*)
+    echo "Usage: ./build.sh [options]"
+    echo "Options:"
+    echo "When called with no options cmake will configure and build the project"
+    echo "-h, --help         print this message"
+    echo "-c, --clean        clean the cmake project"
+    echo "-d, --delete       remove the build directory"
 ;;
 
 esac

b21f5e7bafc1109eaf0b28998a3fad67560008b4
asidhu77 <amritpalsidhu.p@gmail.com>
2022-05-29 21:34:14 -0700

    Modify mechanics equations to use vectors
    More could be done on this

--- particle_sim/src/mechanic_equations.c
+++ particle_sim/src/mechanic_equations.c
@@ -1,9 +1,6 @@
 #include "mechanic_equations.h"
 
 
-static double impulse_integral;
-
-
 double gravitational_force(const double m1, const double m2, const double r)
 {
     return (UNIVERSAL_GRAVITY_CONST * m1 * m2) / (r * r);
@@ -14,14 +11,9 @@ double electric_force(const double q1, const double q2, const double r)
     return (COULOMB_CONST * q1 * q2) / (r * r);
 }
 
-double velocity_induced_by_force(const double F, const double m, const double sample_period)
+vector_3d_t velocity_induced_by_force(vector_3d_t *impulse_integral, const vector_3d_t F, const double m, const double sample_period)
 {
-    impulse_integral += F * sample_period;
+    *impulse_integral = vector_3d__add(*impulse_integral, vector_3d__scale(F, sample_period));
 
-    return impulse_integral / m;
-}
-
-void clear_impulse_integral(void)
-{
-    impulse_integral = 0;
+    return vector_3d__scale(*impulse_integral, 1/m);
 }
